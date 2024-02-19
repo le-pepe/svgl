6fca53148441b45e6ff61f874ea42bcf00f20b19
Pepe <jose@rocketbot.com>
2024-02-19 15:12:38 -0300

    🔒 chore(categories/server.ts): comment out rate limiting functionality for now to disable it temporarily 🔧 refactor(categories/server.ts): remove rate limiting logic to simplify the code and improve readability

--- src/routes/api/categories/+server.ts
+++ src/routes/api/categories/+server.ts
@@ -1,7 +1,7 @@
 import type { RequestEvent } from './$types';
 
 import { json } from '@sveltejs/kit';
-import { ratelimit } from '@/server/redis';
+// import { ratelimit } from '@/server/redis';
 
 // Data:
 import { svgs } from '@/data/svgs';
@@ -10,20 +10,20 @@ export const GET = async ({ request }: RequestEvent) => {
   const categories = svgs
     .map((svg) => svg.category)
     .filter((category, index, array) => array.indexOf(category) === index);
-  const ip = request.headers.get('x-forwarded-for') ?? '';
-  const { success, reset } = await ratelimit.limit(ip);
+  // const ip = request.headers.get('x-forwarded-for') ?? '';
+  // const { success, reset } = await ratelimit.limit(ip);
 
-  // Error 429 | If rate limit is exceeded:
-  if (!success) {
-    const now = Date.now();
-    const retryAfter = Math.floor((reset - now) / 1000);
-    return new Response('Too Many Requests', {
-      status: 429,
-      headers: {
-        'Retry-After': retryAfter.toString()
-      }
-    });
-  }
+  // // Error 429 | If rate limit is exceeded:
+  // if (!success) {
+  //   const now = Date.now();
+  //   const retryAfter = Math.floor((reset - now) / 1000);
+  //   return new Response('Too Many Requests', {
+  //     status: 429,
+  //     headers: {
+  //       'Retry-After': retryAfter.toString()
+  //     }
+  //   });
+  // }
 
   // Status 200 | If limit is a number:
   return json(
